c9d3df5e22336862f6aaac3c15312983045ea47c
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2022-12-08 09:58:06 +0000

    Use `moment.utc` constructor instead of `moment` constructor to avoid interference from device timezone.

--- src/rendering/filters/dateFilter.js
+++ src/rendering/filters/dateFilter.js
@@ -1,7 +1,7 @@
 import moment from "moment";
 
 export default function dateFilter(str, format, locale = "en-gb") {
-  const localMoment = moment(str);
+  const localMoment = moment.utc(str);
   localMoment.locale(locale);
   return localMoment.format(format);
 }
