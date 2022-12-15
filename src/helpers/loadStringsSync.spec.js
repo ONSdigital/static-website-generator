6d8a19bfbf6813c8a47d7b60733c290cb61882c4
Lea Hayes <lea.hayes@ext.ons.gov.uk>
2022-12-15 11:20:59 +0000

    Support nested strings directory structure.

--- src/helpers/loadStringsSync.spec.js
+++ src/helpers/loadStringsSync.spec.js
@@ -6,22 +6,30 @@ describe("loadStringsSync(stringsPath)", () => {
 
     expect(strings).toEqual({
       en: {
-        main: {
+        "main": {
           "exampleWithKey": "Keys are good for situations where there are large volumes of text...",
           "exampleWithNullValue": null,
         },
+
+        "nested/directory/example": {
+          "exampleFromNestedDirectory": "An example string from a nested directory.",
+        }
       },
 
       cy: {
-        main: {
+        "main": {
           "exampleWithKey": "Mae allweddi yn dda ar gyfer sefyllfaoedd lle mae llawer iawn o destun...",
           "exampleWithNullValue": null,
         },
 
-        panel: {
+        "panel": {
           "Hide panel": "Cuddio'r panel",
           "Show panel": "Dangos panel",
         },
+
+        "nested/directory/example": {
+          "exampleFromNestedDirectory": "Llinyn enghreifftiol o gyfeiriadur nythu.",
+        }
       },
     });
   });
