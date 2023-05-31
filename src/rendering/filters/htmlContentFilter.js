9a7572a877f6f506f327dddeb3038717cdac1932
Alessio <alessio.venturini@and.digital>
2023-05-31 11:24:11 +0100

    updated breakpoint

--- src/rendering/filters/htmlContentFilter.js
+++ src/rendering/filters/htmlContentFilter.js
@@ -34,7 +34,7 @@ export default function createHtmlContentFilter(data) {
     },
 
     // Decorate table elements with ONS design system classes.
-    [escape("<table>")]: '<div class="ons-table-scrollable__content@m"><table class="ons-table ons-table--scrollable">',
+    [escape("<table>")]: '<div class="ons-table-scrollable__content"><table class="ons-table ons-table--scrollable">',
     [escape("</table>")]: '</table><div>',
     [escape("<thead>")]: '<thead class="ons-table__head">',
     [escape("<tbody>")]: '<tbody class="ons-table__body">',
