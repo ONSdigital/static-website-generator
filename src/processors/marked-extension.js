739476fb0babae383e354ef601c83cc512d3a99c
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2023-11-03 10:42:44 +0000

    add comments to the extension class

--- src/processors/marked-extension.js
+++ src/processors/marked-extension.js
@@ -1,13 +1,22 @@
+// This code defines a custom renderer that extends marked.js and overrides the table rendering method to include a <caption>. 
+// You can learn more about custom renderer here at https://marked.js.org/using_pro#renderer
 export default function markedExtension() {
+  // Declare the heading text so it can be used globally
   let headingText;
   return {
     renderer: {
+      // Custom renderer for headings
       heading(text) {
+        // Set the heading text
         headingText = text;
+      // Return an empty string to skip rendering the heading itself
         return ''; 
-      }, 
+      },       
+      // Custom renderer for tables
       table(header, body){
-        const caption = headingText ? `<caption class="ons-table__caption ons-u-fs-m">${headingText}</caption>` : '';
+        // Check if there is heading text so we can add a caption
+        const caption = headingText ? `<caption>${headingText}</caption>` : '';
+        // Construct the table element with the optional caption
         return `<table>
           ${caption}
           <thead>${header}</thead>
