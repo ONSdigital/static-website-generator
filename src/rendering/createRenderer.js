b1eea598654f85a1018e94233c126164ab33549e
Precious Onyenaucheya <precious.onyenaucheya@ons.gov.uk>
2025-06-05 11:57:54 +0100

    add extend filter

--- src/rendering/createRenderer.js
+++ src/rendering/createRenderer.js
@@ -11,42 +11,71 @@ import itemsList_from_navigationItemsFilter from "./filters/itemsList_from_navig
 import setAttributesFilter from "./filters/setAttributesFilter.js";
 import setPropertyFilter from "./filters/setPropertyFilter.js";
 import tojsonFilter from "./filters/tojsonFilter.js";
+import extend from "./filters/extend.js";
 
-const designSystemPath = `${ process.cwd() }/node_modules/@ons/design-system`;
+const designSystemPath = `${process.cwd()}/node_modules/@ons/design-system`;
 const designSystemCdnBaseUrl = "https://cdn.ons.gov.uk/sdc/design-system/";
 
-let designSystemVersion = fs.readJsonSync(`${designSystemPath}/package.json`).version;
+let designSystemVersion = fs.readJsonSync(
+  `${designSystemPath}/package.json`
+).version;
 if (designSystemVersion === "3.0.1") {
-  const projectPackageJson = fs.readJsonSync(`${process.cwd()}/package.json`, { encoding: "utf8" });
-  const designSystemDependency = projectPackageJson.dependencies["@ons/design-system"];
+  const projectPackageJson = fs.readJsonSync(`${process.cwd()}/package.json`, {
+    encoding: "utf8",
+  });
+  const designSystemDependency =
+    projectPackageJson.dependencies["@ons/design-system"];
   designSystemVersion = designSystemDependency.match(/#(\d+\.\d+\.\d+)$/)[1];
 }
 
 nunjucks.configure(null, {
   watch: false,
-  autoescape: true
+  autoescape: true,
 });
 
 export default async function createRenderer(data, setupNunjucks = null) {
-  const searchPaths = [ ...data.site.templateSearchPaths, `${designSystemPath}`, `${designSystemPath}/src` ];
+  const searchPaths = [
+    ...data.site.templateSearchPaths,
+    `${designSystemPath}`,
+    `${designSystemPath}/src`,
+  ];
 
   const nunjucksLoader = new nunjucks.FileSystemLoader(searchPaths);
   const nunjucksEnvironment = new nunjucks.Environment(nunjucksLoader);
 
   nunjucksEnvironment.addGlobal("designSystemVersion", designSystemVersion);
-  nunjucksEnvironment.addGlobal("designSystemCdnBaseUrl", designSystemCdnBaseUrl);
-  nunjucksEnvironment.addGlobal("designSystemCdnUrl", designSystemCdnBaseUrl + designSystemVersion);
+  nunjucksEnvironment.addGlobal(
+    "designSystemCdnBaseUrl",
+    designSystemCdnBaseUrl
+  );
+  nunjucksEnvironment.addGlobal(
+    "designSystemCdnUrl",
+    designSystemCdnBaseUrl + designSystemVersion
+  );
 
   nunjucksEnvironment.addFilter("date", dateFilter);
   nunjucksEnvironment.addFilter("htmlContent", createHtmlContentFilter(data));
-  nunjucksEnvironment.addFilter("isInternalUrl", createIsInternalUrlFilter(data));
-  nunjucksEnvironment.addFilter("itemsList_from_navigation", itemsList_from_navigationFilter);
-  nunjucksEnvironment.addFilter("itemsList_from_navigationItems", itemsList_from_navigationItemsFilter);
-  nunjucksEnvironment.addFilter("localize", createLocalizeFilter(data.site, data.stringsByLanguage));
+  nunjucksEnvironment.addFilter(
+    "isInternalUrl",
+    createIsInternalUrlFilter(data)
+  );
+  nunjucksEnvironment.addFilter(
+    "itemsList_from_navigation",
+    itemsList_from_navigationFilter
+  );
+  nunjucksEnvironment.addFilter(
+    "itemsList_from_navigationItems",
+    itemsList_from_navigationItemsFilter
+  );
+  nunjucksEnvironment.addFilter(
+    "localize",
+    createLocalizeFilter(data.site, data.stringsByLanguage)
+  );
   nunjucksEnvironment.addFilter("setAttributes", setAttributesFilter);
   nunjucksEnvironment.addFilter("setProperty", setPropertyFilter);
   nunjucksEnvironment.addFilter("tojson", tojsonFilter);
   nunjucksEnvironment.addFilter("uniqueId", createUniqueIdFilter());
+  nunjucksEnvironment.addGlobal("extend", extend);
 
   setupNunjucks?.call(null, { nunjucksEnvironment });
 
@@ -56,10 +85,19 @@ export default async function createRenderer(data, setupNunjucks = null) {
     async render(page, context = {}) {
       try {
         const templateName = `${page.layout}.njk`;
-        return nunjucksEnvironment.render(templateName, { ...data, page, ...context });
-      }
-      catch (err) {
-        const renderError = new Error(`A problem occurred whilst rendering page '${data?.site?.baseUrl ?? "/"}${page.uri}'\n\nlayout: ${page.layout}\nsite: ${data?.site?.name ?? "(undefined)"}`);
+        return nunjucksEnvironment.render(templateName, {
+          ...data,
+          page,
+          ...context,
+        });
+      } catch (err) {
+        const renderError = new Error(
+          `A problem occurred whilst rendering page '${
+            data?.site?.baseUrl ?? "/"
+          }${page.uri}'\n\nlayout: ${page.layout}\nsite: ${
+            data?.site?.name ?? "(undefined)"
+          }`
+        );
         renderError.name = "RenderError";
         renderError.cause = err;
         throw renderError;
@@ -68,14 +106,20 @@ export default async function createRenderer(data, setupNunjucks = null) {
 
     async renderString(templateString, context = {}) {
       try {
-        return nunjucksEnvironment.renderString(templateString, { ...data, ...context });
-      }
-      catch (err) {
-        const renderError = new Error(`A problem occurred whilst rendering string for site '${data?.site?.name ?? "(undefined)"}'.`);
+        return nunjucksEnvironment.renderString(templateString, {
+          ...data,
+          ...context,
+        });
+      } catch (err) {
+        const renderError = new Error(
+          `A problem occurred whilst rendering string for site '${
+            data?.site?.name ?? "(undefined)"
+          }'.`
+        );
         renderError.name = "RenderError";
         renderError.cause = err;
         throw renderError;
       }
     },
-  }
+  };
 }
